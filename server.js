e97df2dd787d796f4847da3ebb321af3a22ed814
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-09-10 01:57:39 +0300

    Server with stage-one end points created

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const url = require('url');
+const { DateTime } = require('luxon');
+
+/*  Payload requirments
+    {
+        "slack_name": "example_name",
+        "current_day": "Monday",
+        "utc_time": "2023-08-21T15:04:05Z",
+        "track": "backend",
+        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+        "github_repo_url": "https://github.com/username/repo",
+        "status_code": 200
+    }
+*/
+
+console.log(DateTime.now().toISO());
+
+
+const server = http.createServer((req, res) => {
+        console.log('Server started');
+        let params = url.parse(req.url, true).query
+
+        let data = {
+            "slack_name": params.slack_name,
+            "current_day": DateTime.now().day,
+            "utc_time": DateTime.now().toISO(),
+            "track": params.track,
+            "github_file_url": "https://github.com/JimmyKurui/HNGx-Backend/blob/main/server.py",
+            "github_repo_url": "https://github.com/JimmyKurui/HNGx-Backend",
+            "status_code": res.statusCode
+        };
+
+        res.end(JSON.stringify(data));
+    }
+);
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for requests...')
+});
