f85612aad81062ad701524325a9c46bec179afe7
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-09-11 23:56:21 +0300

    Using Date

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const moment = require('moment');
 
 const utc_time = moment.utc().format() 
 const { DateTime } = require('luxon');
+const utcTime = new Date(Date.now()).toISOString().split('.')[0] + 'Z';
 
 port = process.env.PORT || 3000;
 /*  Payload requirments
@@ -33,7 +34,7 @@ const server = http.createServer((req, res) => {
                     "slack_name": params.slack_name,
                     "current_day": DateTime.now().weekdayLong,
                     // "utc_time": DateTime.now().plus({minutes: 2}),    // Offset for +/- minutes
-                    "utc_time": utc_time,    // Offset for +/- minutes
+                    "utc_time": utcTime,    // Offset for +/- minutes
                     "track": params.track,
                     "github_file_url": "https://github.com/JimmyKurui/HNGx-Backend/blob/stage-one/server.js",
                     "github_repo_url": "https://github.com/JimmyKurui/HNGx-Backend/tree/stage-one",
